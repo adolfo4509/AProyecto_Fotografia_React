ebb976a771c4ded5cbffc8798916b431e7361282
adolfo4509 <calvo330906@gmail.com>
2021-11-11 18:21:31 -0500

    as avances

--- client/src/views/Contactanos.jsx
+++ client/src/views/Contactanos.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/contactanos.css";
-import { boostrap } from "bootstrap";
 
 const Contactanos = () => {
   return (
